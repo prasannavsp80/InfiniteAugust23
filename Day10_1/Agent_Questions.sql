2d8c20ef33e7e2aa35c72a0c14a2040c199c5ab3
PrasananTrainer03 <PrasananTrainer03>
2023-09-05 14:16:54 +0530

    code

---
+++ Day10_1/Agent_Questions.sql
@@ -0,0 +1,17 @@
+-- Display all records from Agent table
+
+-- Display agentid, firstName,lastname,city,state from Agent Table
+
+-- Display All records from Policy Table
+
+-- Display PolicyId, APpNumber,AnnualPremium,PaymentModeId from Policy Table
+
+-- Display max. annualpremium
+
+-- Display appnumber for which you have max. annual premium 
+
+-- Display AgentId, FirstName,LastName,City,MaritalStatus as 
+-- if maritalStatus is 0 then 'UNMARRIED' 1 THEN 'MARRIED'
+
+-- Display PolicyId, APpNumber,PaymentModeId, as
+-- if paymentModeId is 1 then 'YEARLY' 2 then 'HALFYEARLY' 3 then 'QUARTERLY'
