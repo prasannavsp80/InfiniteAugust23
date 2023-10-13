b1c4da6445f84b1ab3c44e2bd6570ba2a84293db
PrasananTrainer03 <PrasananTrainer03>
2023-10-13 12:28:25 +0530

    code

---
+++ Day36/EmployDaoImpl.java
@@ -0,0 +1,36 @@
+package com.java.ejb;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployDaoImpl  {
+
+	PreparedStatement pst;
+	Connection connection;
+	
+	public List<Employ> showEmployDao() throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select * from Employ";
+		pst = connection.prepareStatement(cmd);
+		ResultSet rs = pst.executeQuery();
+		List<Employ> employList = new ArrayList<Employ>();
+		Employ employ = null;
+		while(rs.next()) {
+			employ = new Employ();
+			employ.setEmpno(rs.getInt("empno"));
+			employ.setName(rs.getString("name"));
+			employ.setGender(Gender.valueOf(rs.getString("gender")));
+			employ.setDept(rs.getString("dept"));
+			employ.setDesig(rs.getString("desig"));
+			employ.setBasic(rs.getDouble("basic"));
+			employList.add(employ);
+		}
+		return employList;
+
+	}
+	
+}
