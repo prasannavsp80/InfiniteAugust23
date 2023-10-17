7881aa1562ee20f6ab942abd41d8d9bf15f8ee19
PrasananTrainer03 <PrasananTrainer03>
2023-10-17 15:22:38 +0530

    code

---
+++ Day37_Part3/RemoteHelper.java
@@ -0,0 +1,58 @@
+package com.java.ejb;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class RemoteHelper {
+
+
+
+	private static Context createInitialContext() throws NamingException {
+        Properties jndiProperties = new Properties();
+        jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, 
+          "org.jboss.naming.remote.client.InitialContextFactory");
+        jndiProperties.put(Context.URL_PKG_PREFIXES, 
+          "org.jboss.ejb.client.naming");
+        jndiProperties.put(Context.PROVIDER_URL, 
+           "http-remoting://localhost:8080");
+        jndiProperties.put("jboss.naming.client.ejb.context", true);
+        return new InitialContext(jndiProperties);
+    }
+	   public static EmployJdbcBeanRemote lookupRemoteStatelessEmploy() throws NamingException {
+		   Context ctx = createInitialContext();
+	        // The app name is the application name of the deployed EJBs. This is typically the ear name
+	        // without the .ear suffix. However, the application name could be overridden in the application.xml of the
+	        // EJB deployment on the server.
+	        // Since we haven't deployed the application as a .ear, the app name for us will be an empty string
+	    //    final String appName = "";
+	        // This is the module name of the deployed EJBs on the server. This is typically the jar name of the
+	        // EJB deployment, without the .jar suffix, but can be overridden via the ejb-jar.xml
+	        // In this example, we have deployed the EJBs in a jboss-as-ejb-remote-app.jar, so the module name is
+	        // jboss-as-ejb-remote-app
+	      //  final String moduleName = "EJBModule";
+	        // AS7 allows each deployment to have an (optional) distinct name. We haven't specified a distinct name for
+	        // our EJB deployment, so this is an empty string
+	        final String distinctName = "";
+	        // The EJB name which by default is the simple class name of the bean implementation class
+	        final String beanName = "EmployJdbcBean";
+	        System.out.println(beanName);
+	        // the remote view fully qualified class name
+	   //     System.out.println(RemoteCalculator.class.getName());
+	        final String viewClassName = EmployJdbcBeanRemote.class.getName();
+	        // let's do the lookup
+	   //     String namespace="ejb:/HelloWorldEJB!com.ejb.HelloWorld";
+	     //   return (RemoteCalculator) context.lookup("ejb:" + appName + "/" + moduleName + "/" + distinctName + "/" + beanName + "!" + viewClassName);
+	        String appName = "";
+            String moduleName = "EmployEjbJdbc";
+          //  String distinctName = "";
+           // String beanName = CalculatorBean.class.getSimpleName();
+           // String viewClassName = RemoteCalculator.class.getName();
+            return (EmployJdbcBeanRemote) ctx.lookup("ejb:" 
+              + appName + "/" + moduleName 
+              + "/" + distinctName + "/" + beanName + "!" + viewClassName);
+	    }
+
+}
