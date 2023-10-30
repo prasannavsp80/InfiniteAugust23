875a12d945ec41d2635d6582be0acafa044483a7
PrasananTrainer03 <PrasananTrainer03>
2023-10-30 11:40:48 +0530

    code

---
+++ Day47_2/EjbEntityExample/ejbModule/com/java/ejb/EmployBeanRemote.java
@@ -0,0 +1,14 @@
+package com.java.ejb;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface EmployBeanRemote {
+	  String addEmploy(Employ Employ);
+	  List<Employ> showEmploy();
+	  Employ searchEmploy(int id);
+	  String deleteEmploy(int id);
+	  String updateEmploy(Employ EmployNew);
+}
